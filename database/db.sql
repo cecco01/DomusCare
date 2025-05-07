fb27a5d0c4806311e5bb4fcd1e893b7d729898af
lrotelli01 <l.rotelli@studenti.unipi.it>
2025-05-07 16:37:26 +0200

    coap

--- database/db.sql
+++ database/db.sql
@@ -54,6 +54,7 @@ CREATE TABLE dispositivi (
     tipo VARCHAR(255) NOT NULL,
     stato INT NOT NULL DEFAULT 0,  -- 2=pronto, 1=attivo, 0=inattivo
     consumo_kwh FLOAT NOT NULL,    -- Consumo in kWh
+    timestamp_attivazione TIMESTAMP DEFAULT NULL, -- Timestamp di attivazione
     durata INT NOT NULL            -- Durata in minuti
 );
 
@@ -62,4 +63,41 @@ INSERT INTO dispositivi (nome, tipo, stato, consumo_kwh, durata) VALUES
 ('Lavatrice', 'Lavatrice', 0, 1.5, 60),
 ('Asciugatrice', 'Asciugatrice', 0, 2.0, 90),
 ('Lavastoviglie', 'Lavastoviglie', 0, 1.2, 45);
+
+-- Abilita il gestore di eventi MySQL (se non è già abilitato)
+SET GLOBAL event_scheduler = ON;
+
+-- Crea un evento per aggiornare lo stato dei dispositivi
+CREATE EVENT aggiorna_stato_dispositivi
+ON SCHEDULE EVERY 1 MINUTE
+DO
+BEGIN
+    UPDATE dispositivi
+    SET stato = 0, timestamp_attivazione = NULL
+    WHERE stato = 1 AND TIMESTAMPDIFF(MINUTE, timestamp_attivazione, NOW()) >= durata;
+    
+END;
+----  SERVE PER RENDERE PIU' LOGICA LA SIMULAZIONE DEI SENSOR
+-- Creazione del trigger per aggiornare il consumo totale
+DELIMITER $$
+
+CREATE TRIGGER aggiorna_consumo_dispositivi
+AFTER INSERT ON data
+FOR EACH ROW
+BEGIN
+    -- Calcola il consumo totale dei dispositivi attivi
+    DECLARE consumo_totale FLOAT;
+    SELECT SUM(consumo_kwh) INTO consumo_totale
+    FROM dispositivi
+    WHERE stato = 1;
+
+    -- Aggiungi il consumo totale al valore di power nella nuova riga
+    IF consumo_totale IS NOT NULL THEN
+        UPDATE data
+        SET power = power + consumo_totale
+        WHERE id = NEW.id;
+    END IF;
+END$$
+
+DELIMITER ;
 
