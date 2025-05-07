0fe83811b27be4c91ae0ba1f9238ef144523f205
cecco01 <ceccarelli.leonardo01@gmail.com>
2025-05-07 14:08:32 +0200

    CoAP

---
+++ CoAP-sensors/resources/res-voltage-status.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "coap-engine.h"
+
+#include "voltage_status.h"
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+
+RESOURCE(res_voltage_status,"title=\"Coap Voltage Status\";rt=\"voltage_status\"",res_get_handler,NULL,res_put_handler,NULL);
+
+static void
+res_put_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
+    size_t len = 0;
+    const char *value = NULL;
+    int success = 1;
+
+    if ((len = coap_get_post_variable(request, "value", &value))){
+        if (strncmp(value, "0", len) == 0){
+            status = 0;
+        }
+        else if (strncmp(value, "1", len) == 0){
+            status = 1;
+        }
+        else{
+            success = 0;
+        }
+    }
+    else{
+        success = 0;
+    }
+
+    if (success){
+        coap_set_status_code(response, CHANGED_2_04);
+        LOG_INFO("Voltage status changed to %d\n", status);
+    }
+    else{
+        coap_set_status_code(response, BAD_REQUEST_4_00);
+        LOG_ERR("Voltage status change failed\n");
+    }
+}
+
+static void
+res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
+    coap_set_header_content_format(response, APPLICATION_JSON);
+    int payload_len = snprintf((char *)buffer, preferred_size, "{\"sensor\":\"voltage\", \"status\":%d}", status);
+    coap_set_payload(response, buffer, payload_len);
+
+    LOG_INFO("Payload: %s\n", buffer);
+}
---
+++ CoAP-sensors/resources/res-voltage.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "coap-engine.h"
+#include "utils/randomize.h"
+#include "math.h"
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_APP
+
+#define MEAN 169.970005
+#define STDDEV 14.283898
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void res_event_handler(void);
+
+EVENT_RESOURCE(res_voltage,"title=\"Observable resource\";voltage",res_get_handler,NULL,NULL,NULL,res_event_handler);
+
+static double current_voltage = 0;
+
+static void res_event_handler(void){
+  current_voltage = generate_gaussian(MEAN, STDDEV);
+  LOG_INFO("Payload to be sent: {\"sensor\":\"voltage\", \"value\":%.2f}\n", current_voltage);
+  coap_notify_observers(&res_voltage);
+}
+
+static void res_get_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset){
+  coap_set_header_content_format(response, APPLICATION_JSON);
+  int payload_len = snprintf((char *)buffer, preferred_size, "{\"sensor\":\"voltage\", \"value\":%.2f}", current_voltage);
+  coap_set_payload(response, buffer, payload_len);
+
+  LOG_INFO("Payload: %s\n", buffer);
+}
