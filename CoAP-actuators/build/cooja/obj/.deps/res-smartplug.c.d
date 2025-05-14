e25f38be2f274d5db70521dc95dece01746de652
lrotelli01 <l.rotelli@studenti.unipi.it>
2025-05-14 15:59:03 +0200

    c

--- CoAP-actuators/build/cooja/obj/.deps/res-smartplug.c.d
+++ CoAP-actuators/build/cooja/obj/.deps/res-smartplug.c.d
@@ -1,5 +1,9 @@
-build/cooja/obj/smartplug.o: smartplug.c \
+build/cooja/obj/res-smartplug.o: resources/res-smartplug.c \
  /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/module-macros.h \
+ /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-engine.h \
+ /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap.h \
+ /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-constants.h \
+ /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-conf.h \
  /home/iot_ubuntu_intel/contiki-ng/os/contiki.h \
  /home/iot_ubuntu_intel/contiki-ng/arch/platform/cooja/./contiki-conf.h \
  project-conf.h \
@@ -23,10 +27,6 @@ build/cooja/obj/smartplug.o: smartplug.c \
  /home/iot_ubuntu_intel/contiki-ng/arch/platform/cooja/./sys/cooja_mt.h \
  /home/iot_ubuntu_intel/contiki-ng/os/sys/pt.h \
  /home/iot_ubuntu_intel/contiki-ng/os/sys/clock.h \
- /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-engine.h \
- /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap.h \
- /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-constants.h \
- /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-conf.h \
  /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-transport.h \
  /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-endpoint.h \
  /home/iot_ubuntu_intel/contiki-ng/os/net/ipv6/uip.h \
@@ -40,20 +40,13 @@ build/cooja/obj/smartplug.o: smartplug.c \
  /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-observe.h \
  /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-separate.h \
  /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-observe-client.h \
- /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-blocking-api.h \
- /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-request-state.h \
- ../ML/smart_grid_model_consumption.h \
- /home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_net.h \
- /home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_common.h \
- /home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_log.h \
- /home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_net_common.h \
- ../ML/smart_grid_model_production.h \
  /home/iot_ubuntu_intel/contiki-ng/os/sys/log.h \
- /home/iot_ubuntu_intel/contiki-ng/os/sys/log-conf.h \
- /home/iot_ubuntu_intel/contiki-ng/os/dev/leds.h \
- /home/iot_ubuntu_intel/contiki-ng/os/dev/gpio-hal.h \
- /home/iot_ubuntu_intel/contiki-ng/arch/platform/cooja/./dev/gpio-hal-arch.h
+ /home/iot_ubuntu_intel/contiki-ng/os/sys/log-conf.h
 /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/module-macros.h:
+/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-engine.h:
+/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap.h:
+/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-constants.h:
+/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-conf.h:
 /home/iot_ubuntu_intel/contiki-ng/os/contiki.h:
 /home/iot_ubuntu_intel/contiki-ng/arch/platform/cooja/./contiki-conf.h:
 project-conf.h:
@@ -77,10 +70,6 @@ project-conf.h:
 /home/iot_ubuntu_intel/contiki-ng/arch/platform/cooja/./sys/cooja_mt.h:
 /home/iot_ubuntu_intel/contiki-ng/os/sys/pt.h:
 /home/iot_ubuntu_intel/contiki-ng/os/sys/clock.h:
-/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-engine.h:
-/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap.h:
-/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-constants.h:
-/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-conf.h:
 /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-transport.h:
 /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-endpoint.h:
 /home/iot_ubuntu_intel/contiki-ng/os/net/ipv6/uip.h:
@@ -94,16 +83,5 @@ project-conf.h:
 /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-observe.h:
 /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-separate.h:
 /home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-observe-client.h:
-/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-blocking-api.h:
-/home/iot_ubuntu_intel/contiki-ng/os/net/app-layer/coap/coap-request-state.h:
-../ML/smart_grid_model_consumption.h:
-/home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_net.h:
-/home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_common.h:
-/home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_log.h:
-/home/iot_ubuntu_intel/.local/lib/python3.10/site-packages/emlearn/eml_net_common.h:
-../ML/smart_grid_model_production.h:
 /home/iot_ubuntu_intel/contiki-ng/os/sys/log.h:
 /home/iot_ubuntu_intel/contiki-ng/os/sys/log-conf.h:
-/home/iot_ubuntu_intel/contiki-ng/os/dev/leds.h:
-/home/iot_ubuntu_intel/contiki-ng/os/dev/gpio-hal.h:
-/home/iot_ubuntu_intel/contiki-ng/arch/platform/cooja/./dev/gpio-hal-arch.h:
