abc0cf400e63eb7c9b145311774f6a06f37ff291
lrotelli01 <l.rotelli@studenti.unipi.it>
2025-05-16 15:43:08 +0200

    rimosso menu da cloud

--- cloud_application/main.py
+++ cloud_application/main.py
@@ -1,26 +1,21 @@
 from serverCoAP import CoAPServer
 
+from ClientCoAP import CoapClient
+
+
 def main():
-    print("Scegli modalità di avvio:")
-    print("1. Avvia server CoAP")
-    print("2. Gestione remota (CLI)")
-    scelta = input("Scelta: ")
-    if scelta == "1":
-        host = "::"
-        port = 5683
-        server = CoAPServer(host, port)
-        try:
-            print("CoAP server start")
-            server.listen(10) # 10 è il numero di pacchetti in attesa
-        except KeyboardInterrupt:
-            print("Server Shutdown")
-            server.close()
-            print("Exiting...")
-    elif scelta == "2":
-        from cli import remote_cli
-        remote_cli()
-    else:
-        print("Scelta non valida.")
+    host = "::"
+    port = 5683
+    server = CoAPServer(host, port)
+    client = CoapClient()
+
+    try:
+        print("CoAP server start")
+        server.listen(10)
+    except KeyboardInterrupt:
+        print("Server Shutdown")
+        server.close()
+        print("Exiting...")
 
 if __name__ == "__main__":
     main()
--- cloud_application/prova.py
+++
@@ -1,7 +0,0 @@
-ip_address = "('fd00::204:4:4:4', 5683)"
-
-# Converti la stringa in una tupla
-ip, port = eval(ip_address)
-
-print(ip)   # Output: fd00::204:4:4:4
-print(port) # Output: 5683
